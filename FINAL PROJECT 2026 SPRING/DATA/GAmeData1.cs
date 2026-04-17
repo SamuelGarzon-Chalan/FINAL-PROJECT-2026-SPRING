16b8b25de91fd94a6576f502debc766af2d5713a
samue <samue@MANOLO_EN_HD>
2026-04-17 00:17:46 -0600

    savedatra 1.0

---
+++ FINAL PROJECT 2026 SPRING/DATA/GAmeData1.cs
@@ -0,0 +1,17 @@
+﻿using FinalBattler.Characters;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace FinalBattler.Data
+{
+    public  class GameData1
+    {
+        public List <Fighter> TeamA { get; set; }
+        public List <Fighter> TeamB { get; set; }
+        public int NumberOfRounds { get; set; }
+
+        public DateTime SavedData { get; set; }
+    }
+}
---
+++ FINAL PROJECT 2026 SPRING/DATA/SaveManager1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace FINAL_Battler.Data
+{
+    public class SaveManager
+    {
+        public static string SaveFilePath = "savegame";
+        public static void SaveGame(FinalBattler.Data.GameData1 gameData)
+        {
+            try
+            {
+                string jsonString = JsonSerializer.Serialize(gameData, new JsonSerializerOptions { WriteIndented = true });
+                File.WriteAllText(SaveFilePath, jsonString);
+                Console.WriteLine("Game saved successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error saving game: {ex.Message}");
+            }
+        }
+    }
+}
